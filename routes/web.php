3d4df663ace73964bfd82f7863a4572f8035f684
Samuele Pascolo <samuele.pascolo@beliven.com>
2025-12-04 10:43:56 +0100

    rimossi riferimenti e redirect alla dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\ProfileController;
 use App\Livewire\QuizController;
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -19,12 +21,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+/*
 Route::get('/test', function () {
     QuestionUpdated::dispatch();
 
     return 'Event dispatched';
 });
+*/
 
-Route::get('/play', QuizController::class)->name('quiz.play')->middleware(['auth', 'verified']);
+Route::get('/', QuizController::class)->name('quiz.play')->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
